e689b20e44e82ee277227c17d94aaf963124f7b8
Duong Duc Anh <duongdoican@gmail.com>
2022-10-23 02:41:01 +0700

    update 23-10

--- src/seeders/20221022181936-demo-user.js
+++ src/seeders/20221022181936-demo-user.js
@@ -2,9 +2,41 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Roles', [{
+      nameRole: 'User',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
+    await queryInterface.bulkInsert('Roles', [{
+      nameRole: 'Admin',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
     await queryInterface.bulkInsert('Users', [{
       email: 'duongdoican@gmail.com',
-      password: 'Minlvip123!',
+      password: '$2a$12$RzZbZZG0ZWSXUeUYckoTXu53H.anMcpomtjdc5o0jp7glNIuQLCp6',
+      status: 1,
+      image: 2,
+      roleId: 1,
+      token: "",
+      name: 'New User',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
+    await queryInterface.bulkInsert('Users', [{
+      email: 'nguyenhoangminh@gmail.com',
+      password: '$2a$12$RzZbZZG0ZWSXUeUYckoTXu53H.anMcpomtjdc5o0jp7glNIuQLCp6',
+      status: 1,
+      image: 2,
+      roleId: 2,
+      token: "",
+      name: 'New User',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }], {});
+    await queryInterface.bulkInsert('Users', [{
+      email: 'duongdoican1@gmail.com',
+      password: '$2a$12$RzZbZZG0ZWSXUeUYckoTXu53H.anMcpomtjdc5o0jp7glNIuQLCp6',
       status: 1,
       image: 2,
       roleId: 1,
