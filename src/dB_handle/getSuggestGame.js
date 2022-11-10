6bf7ad02f9c580a7831127da732f79eefee0a10c
Duong Duc Anh <duongdoican@gmail.com>
2022-11-11 01:54:29 +0700

    update

--- src/dB_handle/getSuggestGame.js
+++ src/dB_handle/getSuggestGame.js
@@ -4,7 +4,7 @@ let getSuggestGame = () => {
   return new Promise(async (resolve, reject) => {
     try {
 
-      const users = await db.sequelize.query(`SELECT * FROM Products ORDER BY 37*(UNIX_TIMESTAMP() ^ Products.id) & 0xffff LIMIT 5`);
+      const users = await db.sequelize.query(`SELECT *, Lists.name as type FROM Products LEFT JOIN Lists ON Products.listId = Lists.id ORDER BY 37*(UNIX_TIMESTAMP() ^ Products.id) & 0xffff LIMIT 4`);
       let product = Object.values(JSON.parse(JSON.stringify(users[0])))
 
       resolve(product)
